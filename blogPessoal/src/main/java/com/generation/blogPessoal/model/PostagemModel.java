a1e604f142358e61b4917183ae00b546de737ef5
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-05-28 12:12:55 -0300

    relacao entre tabelas

--- blogPessoal/src/main/java/com/generation/blogPessoal/model/PostagemModel.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/model/PostagemModel.java
@@ -5,12 +5,15 @@ import java.util.Date;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -38,6 +41,10 @@ public class PostagemModel
 		
 		@Temporal(TemporalType.TIMESTAMP)
 		private Date data = new java.sql.Date(System.currentTimeMillis());
+		
+		@ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Tema tema;
 		
 	//encapsulamento
 		public long getId() 
@@ -79,5 +86,17 @@ public class PostagemModel
 		{
 			this.data = data;
 		}
+
+		public Tema getTema() 
+		{
+			return tema;
+		}
+
+		public void setTema(Tema tema) 
+		{
+			this.tema = tema;
+		}
+		
+		
 
 }
